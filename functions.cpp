6cbb8ac4ccc5eb74175fceea029d7363f5956d6f
jtmelookaran-umkc <123521613+jtmelookaran-umkc@users.noreply.github.com>
2025-02-10 20:10:38 -0600

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,14 @@
+//Function 1 
+//Check if a certain integer exists in array and return index 
+
+//Function 2
+//Modify value of integer when called with index, return new and old value 
+
+//Function 3
+//Adds a new integer to the end of an array
+
+//Function 4 
+//Recieves an index and removes the integer at that point
+
+//Second part is to implement try and catch blocks for the first two functions 
+
